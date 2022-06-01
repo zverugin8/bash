641b617ca44e44b60f15aed989da7dd195a28fe5
Soroka Sergey <zverugin8@gmail.com>
2022-06-01 23:24:56 +0300

    hw9 part1

---
+++ hw9_my_service.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+sleep 100 &
+qqq=$(ps -eo pid,cmd | grep sleep) ; echo $qqq ; echo ${#qqq}
+IFS=' '
+read -a strarr <<< "$qqq"
+echo "There are ${#strarr[*]} words in the text."
+for val in "${strarr[@]}";
+do
+  printf "$val\n"
+done
