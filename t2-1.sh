7dccc599026487864aa628deee52cb711a137e03
Soroka Sergey <zverugin8@gmail.com>
2022-06-22 17:11:20 +0300

    task 2 begin

--- t2-1.sh
+++ t2-1.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/bash
-cat ./output.txt | sed 's/not ok/false,/' |  sed 's/  */ /g' | sed 's/ok/true,/' > ./out1.txt
+cat ./output.txt | head -n 1 | sed 's/, /;/g'| sed -E -e 's/\[ | \]//g'  > ./out1.txt
+#cat ./output.txt | tail -n+3 | head -n -2 | sed 's/not ok/false;/' |  sed 's/  */ /g' | sed 's/ok/true;/' |sed -e 's/), /);/'| sed -e 's/ \([0-9]\+\) /\1;/' >> ./out1.txt
+cat ./output.txt | tail -n+3 | head -n -2 | sed 's/not ok/false;/
+                                                 s/  */ /g
+                                                 s/ok/true;/
+                                                 s/), /);/
+                                                 s/ \([0-9]\+\) /\1;/' >> ./out1.txt
+cat ./output.txt | tail -n 1 | sed 's/ (of / success;/
+                                    s/) tests passed/ total/
+                                    s/tests failed/failed/
+                                    s/ rated as / rating /
+                                    s/, /;/g' | \
+                                    sed -r 's/([1-9]+) ([a-zA-Z]+)/\2 \1/g' >> ./out1.txt
+cat ./out1.txt
+#cat ./out1.txt | awk -F ";" '{print $1}'
 #cat ./output.txt | sed 's/not ok/nok/' | sed -n 3p |  sed 's/  */ /g'
 arr=()
 str_arr=()
 readarray -t arr < ./out1.txt
 str_arr=( ${arr[2]} )
-str_arr[1]+=","
-echo ${str_arr[@]}
+#str_arr[1]+=","
+#echo ${str_arr[@]}
 
 
 
@@ -21,7 +35,32 @@ echo ${str_arr[@]}
 
 
 
+function inst_jq()
+{
+yum --version > /dev/null 2>&1
+local os
+if [[ $? -eq 0 ]] ; then os="cent7" ; fi
+apt -v > /dev/null 2>&1
+if [[ $? -eq 0 ]] ; then os="ubuntu" ; fi
+dnf --version > /dev/null 2>&1
+if [[ $? -eq 0 ]] ; then os="cent8" ; fi
+echo $os
+case "$os" in
+ubuntu)
+sudo apt install jq -y
+;;
+cent7)
+sudo yum install -y epel-release
+sudo yum install -y jq
+;;
+cent8)
+sudo dnf install -y epel-release
+sudo dnf install -y jq
+esac
+}
+#inst_jq
 
+vjq=$(rpm -q jqa) ; if [[ -n $vjq ]] ; then echo $vjq ; fi
 
 
 
@@ -37,6 +76,10 @@ done <<< "$(cat ./output.txt)"
 echo "${str_arr1[@]}"
 }
 function test1() {
+jq --help > /dev/null 2>&1 ; if [[ $? -ne 0 ]] ; then
+echo "Jq not found, installing jq"
+inst_jq
+fi
 BUCKET_NAME=testbucket
 OBJECT_NAME=testworkflow-2.0.1.jar
 TARGET_LOCATION=/opt/test/testworkflow-2.0.1.jar
@@ -48,3 +91,4 @@ JSON_STRING=$(jq -n \
                    '$ARGS.named')
 echo $JSON_STRING
 }
+test1
