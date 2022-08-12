9f0eebacca559d4e787bc015fa284765b1c64928
Soroka Sergey <zverugin8@gmail.com>
2022-08-12 22:29:47 +0300

    test

--- hw10_1.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/bash
-mkdir ./Sergey_Soroka && touch ./Sergey_Soroka/my_file && echo Hello >./Sergey_Soroka/my_file && cat ./Sergey_Soroka/my_file || echo Something went wrong
--- hw10_2.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/bash
-cat ./passwd | grep "/usr/bin/false" | sed 's!/usr/bin/false!/bin/bash!'
